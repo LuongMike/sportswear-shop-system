3d13d1a54717701059169e494e3d2b92f130ca7e
LuongMike <luongdvse181578@fpt.edu.vn>
2026-01-19 14:01:53 +0700

    Initial project structure with frontend and backend

---
+++ backend/src/main/java/com/team6/ecommercesystem/EcommercesystemApplication.java
@@ -0,0 +1,13 @@
+package com.team6.ecommercesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercesystemApplication.class, args);
+	}
+
+}
