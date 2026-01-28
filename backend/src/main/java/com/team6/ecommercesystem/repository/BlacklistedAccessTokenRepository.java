af6846909f0405da1f7199adb0af8d9ff5badfd2
LuongMike <luongdvse181578@fpt.edu.vn>
2026-01-28 23:01:21 +0700

    feat(domain): add user, role and token models with repositories

--- backend/src/main/java/com/team6/ecommercesystem/repository/BlacklistedAccessTokenRepository.java
+++ backend/src/main/java/com/team6/ecommercesystem/repository/BlacklistedAccessTokenRepository.java
@@ -1,4 +1,9 @@
 package com.team6.ecommercesystem.repository;
 
-public interface BlacklistedAccessTokenRepository {
+import com.team6.ecommercesystem.model.BlackListedAccessToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlacklistedAccessTokenRepository extends CrudRepository<BlackListedAccessToken, String> {
 }
