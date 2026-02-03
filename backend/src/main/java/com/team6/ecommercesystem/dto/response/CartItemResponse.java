a3d0a823f28027cb46b021d668eafb589272f878
LuongMike <luongdvse181578@fpt.edu.vn>
2026-02-04 03:15:42 +0700

    feat(cart): add cart DTOs

--- backend/src/main/java/com/team6/ecommercesystem/dto/response/CartItemResponse.java
+++ backend/src/main/java/com/team6/ecommercesystem/dto/response/CartItemResponse.java
@@ -1,4 +1,19 @@
 package com.team6.ecommercesystem.dto.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class CartItemResponse {
+    private Long id;            // CartItem ID
+    private Long variantId;     // Variant ID
+    private String productName;
+    private String size;
+    private String color;
+    private Double price;
+    private Integer quantity;
+    private Double subTotal;    // price * quantity
+    private String imageUrl;    // Ảnh đại diện của variant
+    private Integer maxStock;
 }
--- backend/src/main/java/com/team6/ecommercesystem/dto/response/CartResponse.java
+++ backend/src/main/java/com/team6/ecommercesystem/dto/response/CartResponse.java
@@ -1,4 +1,15 @@
 package com.team6.ecommercesystem.dto.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class CartResponse {
+    private Long id;
+    private Double totalPrice;
+    private Integer totalItems;
+    private List<CartItemResponse> items;
 }
