70a57c9fde62c9dc18ca790da4c5917c802b5f8c
LuongMike <luongdvse181578@fpt.edu.vn>
2026-02-04 01:30:07 +0700

    feat(product): add product request DTOs

--- backend/src/main/java/com/team6/ecommercesystem/dto/request/VariantRequest.java
+++ backend/src/main/java/com/team6/ecommercesystem/dto/request/VariantRequest.java
@@ -1,4 +1,20 @@
 package com.team6.ecommercesystem.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VariantRequest {
+    private String size;
+    private String color;
+    private Double price;
+    private Integer stockQuantity;
+    private String sku;
+    private List<String> imageUrls;
 }
