2bb36c53c306bce7d26ed5dab3f5fd54cc76d9d6
LuongMike <luongdvse181578@fpt.edu.vn>
2026-02-04 01:30:21 +0700

    feat(product): add product response DTOs

--- backend/src/main/java/com/team6/ecommercesystem/dto/response/ProductSummaryResponse.java
+++ backend/src/main/java/com/team6/ecommercesystem/dto/response/ProductSummaryResponse.java
@@ -1,4 +1,18 @@
 package com.team6.ecommercesystem.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductSummaryResponse {
+    private Long id;
+    private String productName;
+    private String categoryName;
+    private String brandName;
+    private String sportName;
 }
