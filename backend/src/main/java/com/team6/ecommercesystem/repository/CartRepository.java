a322cb486fcc9b93adc66673d78dfb7add012630
LuongMike <luongdvse181578@fpt.edu.vn>
2026-02-04 03:15:31 +0700

    feat(cart): add cart repository

--- backend/src/main/java/com/team6/ecommercesystem/repository/CartRepository.java
+++ backend/src/main/java/com/team6/ecommercesystem/repository/CartRepository.java
@@ -1,4 +1,12 @@
 package com.team6.ecommercesystem.repository;
 
-public interface CartRepository {
+import com.team6.ecommercesystem.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart,Long> {
+    Optional<Cart> findByUserId(Long userId);
 }
