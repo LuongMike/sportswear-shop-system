d5256dfcb707f572f3f9a5bc08b64f32ab15f437
LuongMike <luongdvse181578@fpt.edu.vn>
2026-02-04 01:30:39 +0700

    feat(product): implement product service layer

--- backend/src/main/java/com/team6/ecommercesystem/service/ProductService.java
+++ backend/src/main/java/com/team6/ecommercesystem/service/ProductService.java
@@ -1,4 +1,23 @@
 package com.team6.ecommercesystem.service;
 
-public class ProducService {
+import com.team6.ecommercesystem.dto.request.ProductRequest;
+import com.team6.ecommercesystem.dto.request.VariantRequest;
+import com.team6.ecommercesystem.dto.response.ProductDetailResponse;
+import com.team6.ecommercesystem.dto.response.ProductSummaryResponse;
+import com.team6.ecommercesystem.dto.response.VariantResponse;
+import com.team6.ecommercesystem.model.ProductVariant;
+
+import java.util.List;
+
+public interface ProductService {
+    public ProductDetailResponse createProduct(ProductRequest request);
+    public List<ProductSummaryResponse> getAllProducts();
+    public ProductDetailResponse getProductDetail(Long id);
+    public ProductSummaryResponse updateProduct(Long id, ProductRequest request);
+    public void deleteProduct(Long id);
+
+    public VariantResponse addVariant(Long productId, VariantRequest request);
+    public VariantResponse updateVariant(Long id, VariantRequest request);
+    public void deleteVariant(Long id);
+    public void updateStock(Long variantId, Integer quantity);
 }
